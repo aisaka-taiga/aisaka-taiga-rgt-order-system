47fc617f0696bf1b94bac632274899f9588afb4c
aisaka-taiga <qwerty0204@gmail.com>
2025-02-28 14:16:20 +0900

    Spring Initializr & create-react-app

---
+++ backend/src/main/java/com/rgt/order_system/BumjunkimApplication.java
@@ -0,0 +1,13 @@
+package com.rgt.order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumjunkimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumjunkimApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/rgt/order_system/BumjunkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgt.order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumjunkimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
